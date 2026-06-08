947e7639c7b5786b7d1093e51dfaa91f1f4258ca
mac <mac@macs-MacBook-Pro.local>
2026-06-08 15:21:23 +0300

    Add TikTok and Meta pixels to store homepage

--- src/app/[storeSlug]/page.tsx
+++ src/app/[storeSlug]/page.tsx
@@ -64,6 +64,26 @@ export default function StorePage() {
   return (
     <div dir={isRTL ? 'rtl' : 'ltr'} style={{ fontFamily: 'system-ui, -apple-system, Arial, sans-serif', background: '#080808', minHeight: '100vh', color: '#fff', overflowX: 'hidden' }}>
 
+      {store.tiktok_pixel_id && (
+        <script dangerouslySetInnerHTML={{ __html: [
+          "!function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=['page','track','identify','instances','debug','on','off','once','ready','alias','group','enableCookie','disableCookie'];ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};ttq.load=function(e,n){var i='https://analytics.tiktok.com/i18n/pixel/events.js';ttq._i=ttq._i||{};ttq._i[e]=[];ttq._i[e]._u=i;ttq._t=ttq._t||{};ttq._t[e]=+new Date;ttq._o=ttq._o||{};ttq._o[e]=n||{};var o=document.createElement('script');o.type='text/javascript';o.async=!0;o.src=i+'?sdkid='+e+'&lib='+t;var a=document.getElementsByTagName('script')[0];a.parentNode.insertBefore(o,a)};",
+          "ttq.load('" + store.tiktok_pixel_id + "');",
+          "ttq.page();",
+          "}(window,document,'ttq');",
+        ].join('\n') }} />
+      )}
+
+      {store.meta_pixel_id && (
+        <>
+          <script dangerouslySetInnerHTML={{ __html: [
+            "!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');",
+            "fbq('init', '" + store.meta_pixel_id + "');",
+            "fbq('track', 'PageView');",
+          ].join('\n') }} />
+          <noscript><img height="1" width="1" style={{display:'none'}} src={"https://www.facebook.com/tr?id=" + store.meta_pixel_id + "&ev=PageView&noscript=1"} alt="" /></noscript>
+        </>
+      )}
+
       <style>{`
         @keyframes float { 0%, 100% { transform: translateY(0px); } 50% { transform: translateY(-8px); } }
         @keyframes pulse-glow { 0%, 100% { opacity: 0.4; } 50% { opacity: 0.8; } }

0b898de54d62bcf4df3821821287479c3ef9e595
mac <mac@macs-MacBook-Pro.local>
2026-06-08 15:24:54 +0300

    Fix pixel injection using Next.js Script component

--- src/app/[storeSlug]/page.tsx
+++ src/app/[storeSlug]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import Script from 'next/script'
 import { createClient } from '@/lib/supabase/client'
 import { useParams, useRouter } from 'next/navigation'
 
@@ -65,22 +66,53 @@ export default function StorePage() {
     <div dir={isRTL ? 'rtl' : 'ltr'} style={{ fontFamily: 'system-ui, -apple-system, Arial, sans-serif', background: '#080808', minHeight: '100vh', color: '#fff', overflowX: 'hidden' }}>
 
       {store.tiktok_pixel_id && (
-        <script dangerouslySetInnerHTML={{ __html: [
-          "!function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=['page','track','identify','instances','debug','on','off','once','ready','alias','group','enableCookie','disableCookie'];ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};ttq.load=function(e,n){var i='https://analytics.tiktok.com/i18n/pixel/events.js';ttq._i=ttq._i||{};ttq._i[e]=[];ttq._i[e]._u=i;ttq._t=ttq._t||{};ttq._t[e]=+new Date;ttq._o=ttq._o||{};ttq._o[e]=n||{};var o=document.createElement('script');o.type='text/javascript';o.async=!0;o.src=i+'?sdkid='+e+'&lib='+t;var a=document.getElementsByTagName('script')[0];a.parentNode.insertBefore(o,a)};",
-          "ttq.load('" + store.tiktok_pixel_id + "');",
-          "ttq.page();",
-          "}(window,document,'ttq');",
-        ].join('\n') }} />
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
+            !function(w,d,t){
+              w.TiktokAnalyticsObject=t;
+              var ttq=w[t]=w[t]||[];
+              ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];
+              ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
+              for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
+              ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};
+              ttq.load=function(e,n){
+                var i="https://analytics.tiktok.com/i18n/pixel/events.js";
+                ttq._i=ttq._i||{};ttq._i[e]=[];ttq._i[e]._u=i;
+                ttq._t=ttq._t||{};ttq._t[e]=+new Date;
+                ttq._o=ttq._o||{};ttq._o[e]=n||{};
+                var o=document.createElement("script");
+                o.type="text/javascript";o.async=!0;
+                o.src=i+"?sdkid="+e+"&lib="+t;
+                var a=document.getElementsByTagName("script")[0];
+                a.parentNode.insertBefore(o,a)
+              };
+              ttq.load("${store.tiktok_pixel_id}");
+              ttq.page();
+            }(window,document,"ttq");
+          `}
+        </Script>
       )}
 
       {store.meta_pixel_id && (
         <>
-          <script dangerouslySetInnerHTML={{ __html: [
-            "!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');",
-            "fbq('init', '" + store.meta_pixel_id + "');",
-            "fbq('track', 'PageView');",
-          ].join('\n') }} />
-          <noscript><img height="1" width="1" style={{display:'none'}} src={"https://www.facebook.com/tr?id=" + store.meta_pixel_id + "&ev=PageView&noscript=1"} alt="" /></noscript>
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s){
+                if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)
+              }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init','${store.meta_pixel_id}');
+              fbq('track','PageView');
+            `}
+          </Script>
+          <noscript>
+            <img height="1" width="1" style={{display:'none'}}
+              src={"https://www.facebook.com/tr?id=" + store.meta_pixel_id + "&ev=PageView&noscript=1"} alt="" />
+          </noscript>
         </>
       )}
 
